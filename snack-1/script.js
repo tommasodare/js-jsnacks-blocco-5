7631fed06fddb117f6bc34821de208e15e4af857
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-26 14:40:23 +0100

    Done Exercise 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,22 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+// 1. Stampo in console l'array completa di nomi
+console.log(names);
+
+// 2. Utilizzo l'operatore spread(...) per espandere gli elementi dell'array come argomenti separati
+console.log(...names);
+
+// 3. Eseguo il motodo .forEach
+names.forEach((element) => {
+    console.log(element);
+})
+
+// 4. Utilizzo il metodo Map
+
+const newNams = names.map((element) => {
+    return element
+})
+
+console.log(newNams);
