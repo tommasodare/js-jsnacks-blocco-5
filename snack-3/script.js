f8a42ffa114f56e57c2c74f6af7174c708b8169a
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-26 14:57:06 +0100

    Done Exercise 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,11 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+// Utilizzo il metodo Map
+
+const incrementNumber = numbers.map((number) => {
+    return number + 1
+})
+
+console.log(incrementNumber);
